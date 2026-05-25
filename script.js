6a3db5dc804e9d22524b2b710ab3b278cbad2816
Harry Schleiss <Harry.schleiss@hftm.ch>
2026-05-25 15:00:04 +0200

    feat: reaction test und darkmode erweitert

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 function toggleDarkMode() {
     document.body.classList.toggle("darkmode");
+
+    let button = document.getElementById("mode-button");
+    if (document.body.classList.contains("darkmode")) {
+        button.innerHTML = "Light Mode";
+    } else {
+        button.innerHTML = "Dark Mode";
+    }
 }
+
+let startTime;
+function startReactionTest() {
+    const box = document.getElementById("reaction-box");
+    const result = document.getElementById("reaction-result");
+
+    result.innerHTML = "Warte auf Grün...👀";
+    box.style.display = "none";
+
+    let randomTime = Math.random()*3000 +2000; // 2-5 Sekunden
+
+    setTimeout(function() {
+        box.style.display = "block";
+        box.style.backgroundColor = "green";
+        result.innerHTML = "Klick jetzt!👆";
+
+        startTime = Date.now();
+    }, randomTime);
+}
+
+document.getElementById("reaction-box").onclick = function() {
+
+    let endTime = Date.now();
+    let reactionTime = (endTime - startTime) / 1000; // in Sekunden
+
+    document.getElementById("reaction-result").innerHTML = "🏆Deine Reaktionszeit: " + reactionTime.toFixed(3) + " Sekunden💪";
+
+    this.style.display = "none";
+} 
+
